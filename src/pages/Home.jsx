a5af7ae64400a571fa01a4f5e55d60969ce30b1f
Christian <critan_03@live.com>
2023-04-02 17:38:28 -0500

    feat: add home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Carreras from '../components/shared/Carreras';
 
 export const Home = () => {
-  return (
-    <div className='flex flex-wrap items-center justify-evenly gap-x-4 px-4'>Home</div>
-  )
-}
+	return (
+		<div className="flex flex-wrap items-center justify-evenly gap-x-4 px-4">
+			<Carreras />
+		</div>
+	);
+};
